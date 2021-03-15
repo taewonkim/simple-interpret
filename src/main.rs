7489ea9a09079177c6506d17429d4f152387b218
taewonkim <mniktw@gmail.com>
2021-03-15 21:05:30 +0900

    Fix, move expr function to text.rs

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,5 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
     let mut txt = text::Line::new(input);
-    println!("{}", expr(&mut txt));
-}
-
-fn expr(txt: &mut text::Line) -> i32 {
-    let left = (txt.next().to_string()).parse::<i32>().unwrap();
-    let op = txt.next().to_string();
-    let right = (txt.next().to_string()).parse::<i32>().unwrap();
-
-    if op == "+" {
-        left + right
-    }
-    else
-    if op == "-" {
-        left - right
-    }
-    else
-    if op == "*" {
-        left * right
-    }
-    else
-    if op == "/" {
-        left / right
-    }
-    else
-    if op == "%" {
-        left % right
-    }
-    else {
-        0
-    }
+    println!("{}", txt.expr());
 }
--- src/text.rs
+++ src/text.rs
@@ -13,4 +13,34 @@ impl Line {
         self.pos += 1;
         &self.text[tmp..(tmp+1)]
     }
+
+    pub fn expr(&mut self) -> i32 {
+        let left = (self.next().to_string()).parse::<i32>().unwrap();
+        let op = self.next().to_string();
+        let right = (self.next().to_string()).parse::<i32>().unwrap();
+
+        if op == "+" {
+            left + right
+        }
+        else
+        if op == "-" {
+            left - right
+        }
+        else
+        if op == "*" {
+            left * right
+        }
+        else
+        if op == "/" {
+            left / right
+        }
+        else
+        if op == "%" {
+            left % right
+        }
+        else {
+            0
+        }
+    }
 }
+
