e42df61a54a314a1dda7ca11f98b48d71f5132d2
taewonkim <mniktw@gmail.com>
2021-03-20 12:38:59 +0900

    Formatting src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,18 @@ use std::io::{self, Write};
 mod text;
 
 fn main() {
-	let mut input = String::from("");
-	print!("calc> ");
+    let mut input = String::from("");
+
+    print!("calc> ");
+    // 이걸 반드시 해줘야 할까?
     io::stdout()
         .flush()
         .unwrap();
-	io::stdin()
+
+    io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
+
     let mut txt = text::Line::new(input);
     println!("{}", txt.expr());
 }
